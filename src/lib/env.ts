58b60fb33b29460e4136faae70b0514db045af8f
Pranoy Majumdar <works.pranoy@gmail.com>
2025-08-30 00:15:32 +0530

    chore: add NODE_ENV field

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
+    NODE_ENV: z.enum(["development", "test", "production"]),
   },
   client: {},
   // For Next.js >= 13.4.4, you only need to destructure client variables:

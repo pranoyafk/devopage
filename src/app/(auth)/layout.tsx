f63dfc0713b58e0bb68f6f07fb40af0683d40997
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-01 20:14:37 +0530

    refactor(auth): remove unnecessary searchParams

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,12 +3,7 @@ import { headers } from 'next/headers';
 import { notFound } from 'next/navigation';
 import type { ReactNode } from 'react';
 
-export default async function AuthLayout({
-  children,
-}: {
-  children: ReactNode;
-  searchParams: Record<string, string | string[] | undefined>;
-}) {
+export default async function AuthLayout({ children }: { children: ReactNode }) {
   const getSessionData = await auth.api.getSession({
     headers: await headers(),
   });

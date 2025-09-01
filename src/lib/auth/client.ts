74af5739821f9e2331554d1174ff14cdcd97cdce
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-01 20:47:09 +0530

    refactor(auth): remove base url field

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,9 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
 
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
-});
+export const authClient = createAuthClient({});
 
 export type User = (typeof authClient.$Infer.Session)['user'];
 export type Session = (typeof authClient.$Infer.Session)['session'];

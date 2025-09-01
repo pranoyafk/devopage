60e6609e7d2f6d5afca5687bd4a7ff6cb07b83a2
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-02 05:22:38 +0530

    refactor: remove drawer and use media query

--- src/app/(routes)/_components/post-creation-dialog.tsx
+++ src/app/(routes)/_components/post-creation-dialog.tsx
@@ -10,59 +10,24 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer';
-
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { IconLock, IconUsersGroup, IconWorld, type ReactNode } from '@tabler/icons-react';
 import type { User } from '@/lib/auth/client';
 import { UserAvatar } from '@/components/user-avatar';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
-import { useMediaQuery } from '@/hooks/use-media-query';
 
 export function PostCreationDialog({ user, children }: { user: User; children: ReactNode }) {
-  const isDesktop = useMediaQuery('(min-width: 768px)');
-  const Comp = isDesktop
-    ? {
-        Root: Dialog,
-        Trigger: DialogTrigger,
-        Content: DialogContent,
-        Header: DialogHeader,
-        Title: DialogTitle,
-        Description: DialogDescription,
-        Footer: DialogFooter,
-        Close: DialogClose,
-      }
-    : {
-        Root: Drawer,
-        Trigger: DrawerTrigger,
-        Content: DrawerContent,
-        Header: DrawerHeader,
-        Title: DrawerTitle,
-        Description: DrawerDescription,
-        Footer: DrawerFooter,
-        Close: DrawerClose,
-      };
-
   return (
-    <Comp.Root>
-      <Comp.Trigger asChild>{children}</Comp.Trigger>
-      <Comp.Content className="bg-card max-h-screen gap-0 p-0 shadow-lg will-change-transform">
-        <Comp.Header className="items-start border-b px-4 py-3">
-          <Comp.Title className="text-lg">Create New Post</Comp.Title>
-          <Comp.Description className="text-muted-foreground text-start text-sm">
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="gap-0 p-0">
+        <DialogHeader className="items-start border-b px-4 py-3">
+          <DialogTitle className="text-lg">Create New Post</DialogTitle>
+          <DialogDescription className="text-muted-foreground text-start text-sm">
             Share your thoughts with your audience.
-          </Comp.Description>
-        </Comp.Header>
+          </DialogDescription>
+        </DialogHeader>
         <div className="space-y-4 p-4">
           {/* Header */}
           <div className="grid">
@@ -104,13 +69,13 @@ export function PostCreationDialog({ user, children }: { user: User; children: R
             <span className="mt-3 ml-auto block text-xs">10/280</span>
           </div>
         </div>
-        <Comp.Footer className="border-t px-4 py-3">
-          <Comp.Close asChild>
+        <DialogFooter className="border-t px-4 py-3">
+          <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
-          </Comp.Close>
+          </DialogClose>
           <Button>Create Post</Button>
-        </Comp.Footer>
-      </Comp.Content>
-    </Comp.Root>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }

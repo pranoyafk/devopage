e1550fb0b90b1b87ba1e54d77f556a296ae64cb1
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-01 20:14:01 +0530

    fix: eslint config prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', eslintConfigPrettier),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintConfigPrettier,
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },

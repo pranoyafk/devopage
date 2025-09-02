439176026637492682754f8093e1a49747a1d2b9
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-02 05:34:16 +0530

    refactor: remove create port button for unauthorized users

--- src/app/(routes)/_components/sidebar/sidebar-header.tsx
+++ src/app/(routes)/_components/sidebar/sidebar-header.tsx
@@ -8,26 +8,21 @@ export async function SidebarHeader() {
   const { user } = await getAuthSession();
 
   return (
-    <div className="p-4">
-      <div className="mb-6 flex items-center gap-2 px-2">
+    <div className="space-y-6 p-4">
+      <div className="flex items-center gap-2 px-2">
         <div className="bg-primary flex h-8 w-8 items-center justify-center rounded-lg">
           <LucideCode2 className="text-primary-foreground h-4 w-4" />
         </div>
         <span className="text-lg font-semibold">Devopage</span>
       </div>
 
-      {user ? (
+      {user && (
         <PostCreationDialog user={user}>
           <Button size="lg" className="w-full">
             <IconPlus className="mr-2 h-4 w-4" />
             Create Post
           </Button>
         </PostCreationDialog>
-      ) : (
-        <Button size="lg" className="w-full">
-          <IconPlus className="mr-2 h-4 w-4" />
-          Create Post
-        </Button>
       )}
     </div>
   );

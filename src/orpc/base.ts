352c42f48a43352dae84f64904f8d9ed03affd6a
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-05 20:42:31 +0530

    chore: add global not found error

--- src/orpc/base.ts
+++ src/orpc/base.ts
@@ -4,6 +4,9 @@ import { os } from '@orpc/server';
 export const base = os
   .errors({
     UNAUTHORIZED: { message: 'Unauthorized access.' },
+    NOT_FOUND: {
+      message: 'Resource not found',
+    },
   })
   .$context<{
     headers: Headers;

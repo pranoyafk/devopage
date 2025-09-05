cd160c6106d45ae2af93dd34101c0dff5604c158
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-05 19:05:43 +0530

    feat: add date-fns package and a func to format timestamps

---
+++ src/lib/format-timestamp.tsx
@@ -0,0 +1,15 @@
+import { formatDistanceToNow } from 'date-fns';
+
+export function formatTimestamp(date: Date) {
+  const fullText = formatDistanceToNow(date, { addSuffix: true });
+
+  return fullText
+    .replace('about ', '')
+    .replace(' hours', ' hrs')
+    .replace(' hour', ' hr')
+    .replace(' minutes', ' mins')
+    .replace(' minute', ' min')
+    .replace(' seconds', ' secs')
+    .replace(' second', ' sec')
+    .replace('less than a minute', 'just now');
+}

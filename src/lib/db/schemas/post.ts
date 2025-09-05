c25920231ffddb4f1bd2f13a6a13922f9cc89734
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-05 09:42:16 +0530

    feat: add one to one relation for user and post

--- src/lib/db/schemas/post.ts
+++ src/lib/db/schemas/post.ts
@@ -1,16 +1,25 @@
-import { pgEnum, pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 import { user } from './auth';
-import { POST_CONTENT_MAX_LENGTH, VISIBILITY_VALUES } from '@/lib/constants/posts';
+import {
+  POST_CONTENT_MAX_LENGTH,
+  VISIBILITY_VALUES,
+} from '@/lib/constants/posts';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import type z from 'zod';
+import { relations } from 'drizzle-orm';
 
 export const visibilityEnum = pgEnum('visibility', VISIBILITY_VALUES);
 
 export const postsTable = pgTable('posts', {
   id: uuid('id').primaryKey().defaultRandom(),
-  authorId: text('author_id')
-    .notNull()
-    .references(() => user.id, { onDelete: 'cascade' }),
+  authorId: text('author_id').notNull(),
   visibility: visibilityEnum().notNull(),
 
   content: varchar('content', {
@@ -28,6 +37,12 @@ export const postsTable = pgTable('posts', {
     .defaultNow()
     .notNull(),
 });
+export const postsRelations = relations(postsTable, ({ one }) => ({
+  author: one(user, {
+    fields: [postsTable.authorId],
+    references: [user.id],
+  }),
+}));
 
 export const insertPostSchema = createInsertSchema(postsTable).omit({
   id: true,
@@ -38,3 +53,6 @@ export const selectPostSchema = createSelectSchema(postsTable);
 
 export type InsertPostSchema = z.infer<typeof insertPostSchema>;
 export type SelectPostSchema = z.infer<typeof selectPostSchema>;
+export type SelectPostSchemaWithUser = z.infer<typeof selectPostSchema> & {
+  author: typeof user.$inferSelect;
+};

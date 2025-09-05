4143a5c081d8b43aa717564c6e4f03f8d978035c
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-05 09:41:33 +0530

    feat: query with posts author

--- src/orpc/routers/posts/get-all.ts
+++ src/orpc/routers/posts/get-all.ts
@@ -1,10 +1,8 @@
-import { selectPostSchema } from '@/lib/db/schemas';
 import { publicProcedure } from '../../procedures/public';
-import z from 'zod';
 
-export const getAll = publicProcedure
-  .output(z.array(selectPostSchema))
-  .handler(async ({ context }) => {
-    // TODO: Later impliment feed algo
-    return await context.db.query.postsTable.findMany({});
+export const getAll = publicProcedure.handler(async ({ context }) => {
+  // TODO: Later impliment feed algo
+  return await context.db.query.postsTable.findMany({
+    with: { author: true },
   });
+});

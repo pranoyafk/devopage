eb485d48e3a79e68cd20ea9c36f2e256c4ebb668
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-07 19:16:02 +0530

    feat(server): add all necessary env in zod schema for validation

--- apps/server/src/lib/env.ts
+++ apps/server/src/lib/env.ts
@@ -3,6 +3,12 @@ import z from "zod";
 
 export const envSchema = z.object({
   DATABASE_URL: z.url(),
+  BETTER_AUTH_SECRET: z.string().min(32),
+  BETTER_AUTH_URL: z.url(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
 });
 
 declare global {

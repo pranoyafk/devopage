c10c197ffc24cb813b2aadfc36f551f9d654d5ec
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-08 22:57:02 +0530

    chore: format

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: "./src/index.ts",
-  outDir: "build",
-  platform: "node",
-  minify: true,
-  format: "esm",
-  sourcemap: true,
+	entry: "./src/index.ts",
+	outDir: "build",
+	platform: "node",
+	minify: true,
+	format: "esm",
+	sourcemap: true,
 });

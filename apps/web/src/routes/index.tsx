63571713710ae466734e1dc0019b5f7995825f29
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-08 23:00:05 +0530

    feat(web): setup tanstack start with shadcn

---
+++ apps/web/src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <Button>Hello</Button>
+}
